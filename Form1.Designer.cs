970f72640fe3b86af5aa4555af4b351b5930ef0e
Aditya Tiwari <47191499+thesparkilingsun@users.noreply.github.com>
2024-06-27 15:06:57 -0400

    QR CODE DESIGNER

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace BarCodeDesigner2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.inputBtn = new System.Windows.Forms.Button();
+            this.displayImage = new System.Windows.Forms.PictureBox();
+            this.errorLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.displayImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(133, 239);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(296, 22);
+            this.textBox.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // inputBtn
+            // 
+            this.inputBtn.Location = new System.Drawing.Point(190, 293);
+            this.inputBtn.Name = "inputBtn";
+            this.inputBtn.Size = new System.Drawing.Size(160, 23);
+            this.inputBtn.TabIndex = 2;
+            this.inputBtn.Text = "Generate BarCode";
+            this.inputBtn.UseVisualStyleBackColor = true;
+            this.inputBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // displayImage
+            // 
+            this.displayImage.Location = new System.Drawing.Point(133, 52);
+            this.displayImage.Name = "displayImage";
+            this.displayImage.Size = new System.Drawing.Size(296, 168);
+            this.displayImage.TabIndex = 3;
+            this.displayImage.TabStop = false;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Location = new System.Drawing.Point(368, 300);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(0, 16);
+            this.errorLabel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.displayImage);
+            this.Controls.Add(this.inputBtn);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.displayImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button inputBtn;
+        private System.Windows.Forms.PictureBox displayImage;
+        private System.Windows.Forms.Label errorLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using System.Drawing;
+
+using System.Windows.Forms;
+using IronBarCode;
+
+namespace BarCodeDesigner2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            errorLabel.Visible = false; 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConvertImageToBarCode();
+            textBox.Text = "";
+        }
+
+        private void ConvertImageToBarCode()
+        {
+            string data = textBox.Text.ToString();
+            if(data.Length > 0)
+            {
+                errorLabel.Visible = false;
+                var BarCode = BarcodeWriter.CreateBarcode(data, BarcodeEncoding.Code128);
+                BarCode.SaveAsPng("BarCode.png");
+                displayImage.ImageLocation = "G:/C#/BarCodeDesigner2/bin/Debug/BarCode.png";
+            }
+            else
+            {
+                errorLabel.Text = "Enter Input to convert";
+                errorLabel.ForeColor = Color.Red;
+                errorLabel.Visible = true;
+            }
+            
+        }
+    }
+}
